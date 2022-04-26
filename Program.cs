6b9b437f14bf00eedf2cfc12829f1d91024a7fa5
Ivanteev_Kirill <ivanteev87@mail.ru>
2022-04-26 23:26:50 +0300

    Создали программу на задаче

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number1;
+number1 = 6;
 
-
-## Слияние веток##
+if (number1 %2 == 0)
+{
+    Console.WriteLine ($"Число {number1} является четным");
+    
+}
+else
+Console.WriteLine ($"Число {number1} не является четным");
